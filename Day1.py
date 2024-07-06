eceb1c427f5bfb253b1deab732cffcc92567efa9
Purushottam <purushottam01122@gmail.com>
2024-07-06 23:08:59 +0530

    Day2

---
+++ Day1.py
@@ -0,0 +1,45 @@
+# a = 5
+# b = 2
+# print(type(a + b))
+
+##########################################
+# a = "yash"
+# b = "2"
+# print(a + b)
+
+# a = 2.2
+# b = 3.2
+
+# print(a + b) 
+
+
+# a = "Hello World"
+# print(a)
+
+######################################## 
+
+# a = 10
+# b = 20.5
+# print(a + b)
+
+######################################## sep and end  ##################################
+
+# print("Hello", end="\n")
+# print("World")
+
+
+# print("Hello", "World", "World", sep="_")
+
+########################################### format ###################################33
+
+# a = "India"
+# b = "And"
+# print("I love {} {}".format(a, b))
+
+# a = "India"
+# b = "And"
+# print("I love", a, b)
+
+# a = "India"
+# b = "And"
+# print(f"I love {a}, {b}")
